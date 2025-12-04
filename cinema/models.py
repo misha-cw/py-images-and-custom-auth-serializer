651858abd12b23f0589942323d21e71fa3987aba
misha-cw <bigun.mykhaylo@gmail.com>
2025-12-04 15:17:16 +0200

    Fix image upload path in Movie model and update UserAdmin description

--- cinema/models.py
+++ cinema/models.py
@@ -52,7 +52,7 @@ class Movie(models.Model):
     duration = models.IntegerField()
     genres = models.ManyToManyField(Genre)
     actors = models.ManyToManyField(Actor)
-    image = models.ImageField(null=True, upload_to="movie_image_path")
+    image = models.ImageField(null=True, upload_to=movie_image_path)
 
     class Meta:
         ordering = ["title"]
